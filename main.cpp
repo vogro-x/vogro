a0d58d58c57df3b1b078b919142ed25a39dcebc0
Andrewpqc <m13636038496@163.com>
2018-09-04 13:08:33 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include "http_server.hpp"
-// #include "handler.hpp"
 
 int main() {
-    // HTTP 服务运行在 12345 端口，并启用四个线程
-    vogro::Server server(12345,4);
-    // server.user_resource["/"]["GET"]=[](std::ostream& response, vogro::Request& request){
-    //      response << "HTTP/1.1 200 OK\r\nContent-Length: " << "\r\n\r\n";
-    // };
-    server.addRoute("/","GET",[](vogro::Response& response, vogro::Request& request){
-        
-        // std::cout<<"hello,vogro!"<<std::endl;
-        response.addBody("ff");
-        // std::cout<<"in handler"<<std::endl;
-        return;
-    });
+  vogro::Server server(12345, 4);
+  
+  server.addRoute("/", "GET",[](vogro::Response &response, vogro::Request &request) {
+            response.addBody("ff");
+            return;
+        });
 
-    server.customErrorHandler(404,[](vogro::Response& response, vogro::Request& request){
-        response.addBody("not found ....");
-        return;
-    });
-    
-    server.runServer();
-    return 0;
+  server.customErrorHandler(404, [](vogro::Response &response, vogro::Request &request) {
+            response.addBody("not found ....");
+            return;
+      });
+
+  server.runServer();
+  return 0;
 }
