ef0cf2bd1eab1a88d752ab01a9c3b000b43ad89d
Andrewpqc <m13636038496@163.com>
2018-09-06 10:17:16 +0800

    fix issue(#1)

--- utils.hpp
+++ utils.hpp
@@ -61,75 +61,63 @@ std::pair<std::string,std::pair<std::string,std::string>> parse_request_line(std
     return std::make_pair(method,url_version_pair); 
 }
 
-bool urlMatch(std::string requestUrl, std::string handlerUrl, std::map<std::string, std::string> &storeMap) {
-    if (handlerUrl.back() != '/') {
-        handlerUrl += '/';
-    }
+bool urlMatch(std::string requestUrl, std::string handlerUrl,
+              std::map<std::string, std::string> &storeMap) {
+  if (handlerUrl.back() != '/')
+    handlerUrl += '/';
+  if (requestUrl.back() != '/')
+    requestUrl += '/';
 
-    if (requestUrl.back() != '/') {
-        requestUrl += '/';
-    }
-
-    std::string type;
-    std::string name;
-    auto tempIndex = 0;
-    std::string dynamicParam;
-    auto handlerUrlLength=handlerUrl.length();
-    auto requestUrlLength=requestUrl.length();
-    auto max_length = (handlerUrlLength > requestUrlLength) ? handlerUrlLength : requestUrlLength;
-    for (auto i = 0, j = 0; i < max_length; i++, j++) {
-        if (handlerUrl[i] == '{') {
-            auto tempIndex = i + 1;
-            bool flag = true; //true代表当前在type域中
-            do {
-                if (handlerUrl[tempIndex] == ':') {
-                    flag = false;
-                    ++tempIndex;
-                }
-
-                if (!flag) {
-                    name += handlerUrl[tempIndex];
-                }
-                else {
-                    type += handlerUrl[tempIndex];
-                }
-
-                ++tempIndex;
-            } while (handlerUrl[tempIndex] != '}');
-
-            i = tempIndex + 1;
-
-            if (flag == true) {
-                type = "int"; // default type is int
-            }
-
-            //[int:id]   type:int name:id flag=false
-            //[str:username] type:str name:username flag=false
-            //[id]      type:int name:id flag=true
-
-            do {
-                // if type is int, every char in dynamicParam should be in [48,57]
-                if ((type == "int") && (requestUrl[j] < 48 || requestUrl[j] > 57)) {
-                    return false;
-                }
-                dynamicParam += requestUrl[j];
-                ++j;
-            } while (requestUrl[j] != '/');
-
-            // store the dynamic parameters to storeMap
-            storeMap[name] = dynamicParam;
-
-            // clear name, type, dynamicParam
-            name.clear();
-            type.clear();
-            dynamicParam.clear();
+  std::string type, name, dynamicParam;
+  auto handlerUrlLength = handlerUrl.length();
+  auto requestUrlLength = requestUrl.length();
+  auto max_length = (handlerUrlLength > requestUrlLength) ? handlerUrlLength
+                                                          : requestUrlLength;
+  for (auto i = 0, j = 0; (i < max_length) && (j < max_length); i++, j++) {
+    if (handlerUrl[i] == '{') {
+      auto tempIndex = i + 1;
+      bool flag = true; // true代表当前在type域中
+      do {
+        if (handlerUrl[tempIndex] == ':') {
+          flag = false;
+          ++tempIndex;
         }
 
-        if ((i>=handlerUrlLength) ||(j>=requestUrlLength) || (handlerUrl[i] != requestUrl[j])) {
-            return false;
-        }
+        if (!flag)
+          name += handlerUrl[tempIndex];
+        else
+          type += handlerUrl[tempIndex];
+
+        ++tempIndex;
+      } while (handlerUrl[tempIndex] != '}');
+
+      i = tempIndex + 1;
+
+      if (flag == true)
+        type = "int"; // default type is int
+      do {
+        // if type is int, every char in dynamicParam should be in [48,57]
+        if ((type == "int") && (requestUrl[j] < 48 || requestUrl[j] > 57))
+          return false;
+        dynamicParam += requestUrl[j];
+        ++j;
+      } while (requestUrl[j] != '/');
+
+      // store the dynamic parameters to storeMap
+      storeMap[name] = dynamicParam;
+
+      // clear name, type, dynamicParam
+      name.clear();
+      type.clear();
+      dynamicParam.clear();
     }
-    return true;
+
+    if ((i >= handlerUrlLength) || (j >= requestUrlLength) ||
+        (handlerUrl[i] != requestUrl[j])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 }
