43199aee23dd2f67ad63a3ea3d0c4d1faf4189e1
Andrewpqc <m13636038496@163.com>
2018-09-18 12:40:08 +0800

    feat: add basicAuth

--- src/request.hpp
+++ src/request.hpp
@@ -33,7 +33,7 @@ namespace pt = boost::property_tree;
 namespace vogro {
 
 class Request {
-   private:
+private:
     std::string method;
 
     std::string path;
@@ -66,26 +66,26 @@ class Request {
     // the parameters in path
     std::map<std::string, std::string> pathParam;
 
-   public:
+public:
     std::string getMethod() { return this->method; }
 
-    void setMethod(const std::string &mhd) { this->method = mhd; }
+    void setMethod(const std::string& mhd) { this->method = mhd; }
 
     std::string getPath() { return this->path; }
 
-    void setPath(const std::string &ph) { this->path = ph; }
+    void setPath(const std::string& ph) { this->path = ph; }
 
     std::string getVersion() { return this->version; }
 
-    void setVersion(const std::string &vsn) { this->version = vsn; }
+    void setVersion(const std::string& vsn) { this->version = vsn; }
 
     std::string getHost() { return this->host; }
 
-    void setHost(const std::string &hst) { this->host = hst; }
+    void setHost(const std::string& hst) { this->host = hst; }
 
     std::string getRemoteIP() { return this->remote_ip; }
 
-    void setRemoteIP(const std::string &r_ip) { this->remote_ip = r_ip; }
+    void setRemoteIP(const std::string& r_ip) { this->remote_ip = r_ip; }
 
     unsigned short getRemotePort() { return this->remote_port; }
 
@@ -93,61 +93,79 @@ class Request {
 
     std::string getProtocol() { return this->protocol; }
 
-    void setProtocol(const std::string &protoc) { this->protocol = protoc; }
+    void setProtocol(const std::string& protoc) { this->protocol = protoc; }
 
-    std::string getHeader(std::string key) {
+    std::string getHeader(std::string key)
+    {
         auto got = this->headers.find(key);
         return (got == this->headers.end()) ? "" : got->second;
     }
 
-    std::unordered_map<std::string, std::string> getHeaders() {return this->headers; }
+    std::unordered_map<std::string, std::string> getHeaders() { return this->headers; }
 
-    void addHeader(std::string key, std::string val) {this->headers[key] = val;}
+    void addHeader(std::string key, std::string val) { this->headers[key] = val; }
 
-    void setPathParam(std::map<std::string, std::string> &pathParam) {this->pathParam = pathParam; }
+    void setPathParam(std::map<std::string, std::string>& pathParam) { this->pathParam = pathParam; }
 
-    std::string getPathParam(std::string key) {
+    std::string getPathParam(std::string key)
+    {
         auto got = this->pathParam.find(key);
         return (got == this->pathParam.end()) ? "" : got->second;
     }
 
-    void setQueryParam(std::string queryString) {this->queryParam = split_query_string(queryString);}
+    void setQueryParam(std::string queryString) { this->queryParam = split_query_string(queryString); }
 
-    std::string getQueryParam(std::string key) {
+    std::string getQueryParam(std::string key)
+    {
         auto got = this->queryParam.find(key);
         return (got == this->queryParam.end()) ? "" : got->second;
     }
 
-    void setFormParam(std::string formData) {this->formParam = split_query_string(formData);}
+    void setFormParam(std::string formData) { this->formParam = split_query_string(formData); }
 
-    std::string getFormParam(std::string key) {
+    std::string getFormParam(std::string key)
+    {
         auto got = this->formParam.find(key);
         return (got == this->formParam.end()) ? "" : got->second;
     }
 
-    void ReadJSON(std::shared_ptr<std::istream> jsonPtr) {pt::read_json(*jsonPtr, jsonTree); }
+    void ReadJSON(std::shared_ptr<std::istream> jsonPtr) { pt::read_json(*jsonPtr, jsonTree); }
 
-    const pt::ptree &GetJsonTree() { return this->jsonTree; }
+    const pt::ptree& GetJsonTree() { return this->jsonTree; }
 
-    std::string getCookie(std::string key=""){
+    std::string getCookie(std::string key = "")
+    {
         auto cookieString = this->getHeader("Cookie");
-        if(key=="" || cookieString == "")
+        if (key == "" || cookieString == "")
             return cookieString;
 
-         auto tokens =split(cookieString,',');
-         for(auto tk: tokens){
-             auto kv = split(tk,'=');
-             if (kv.size()==2 &&trim(kv[0])==key)
+        auto tokens = split(cookieString, ',');
+        for (auto tk : tokens) {
+            auto kv = split(tk, '=');
+            if (kv.size() == 2 && trim(kv[0]) == key)
                 return kv[1];
-         }
+        }
 
-         return "";
+        return "";
     }
 
-    
-    
+    std::pair<std::string,std::string> basicAuth() {
+        auto auth = this->getHeader("Authorization");
+        if (auth.rfind("Basic ", 0) != 0) 
+            return std::make_pair("","");
+
+        auth =auth.substr(6);
+
+        auto decoded_auth = base64_decode(auth);
+
+        auto result = split(decoded_auth,':');
+        if(result.size()==1)
+            return std::make_pair(result[0],"");
+        
+        return std::make_pair(result[0],result[1]);
+    }
 };
 
-}  // namespace vogro
+} // namespace vogro
 
 #endif
