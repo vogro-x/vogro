4279fd86cb5cbf8b7df584bd48dc8e8ae67c4660
Andrewpqc <m13636038496@163.com>
2018-10-02 18:02:55 +0800

    add static and inline for some util functions

--- src/utils.hpp
+++ src/utils.hpp
@@ -32,8 +32,7 @@
 #define BLUE_COLOR "34"
 #define ZERO_COLOR "0"
 
-std::string ltrim(std::string str)
-{
+inline static std::string ltrim(std::string str) {
     auto length = str.length();
     while (str.length()) {
         if (str[0] == ' ')
@@ -44,8 +43,7 @@ std::string ltrim(std::string str)
     return str;
 }
 
-std::string rtrim(std::string str)
-{
+inline static std::string rtrim(std::string str) {
     auto length = str.length();
     for (auto i = length - 1; i >= 0; i--) {
         if (str[i] == ' ')
@@ -56,7 +54,7 @@ std::string rtrim(std::string str)
     return str;
 }
 
-std::string trim(std::string str) { return rtrim(ltrim(str)); }
+inline static std::string trim(std::string str) { return rtrim(ltrim(str)); }
 
 inline static std::string url_encode(const std::string &value) noexcept {
     static auto hex_chars = "0123456789ABCDEF";
@@ -65,8 +63,10 @@ inline static std::string url_encode(const std::string &value) noexcept {
     result.reserve(value.size()); // Minimum size of result
 
     for (auto &chr : value) {
-        if (!((chr >= '0' && chr <= '9') || (chr >= 'A' && chr <= 'Z') || (chr >= 'a' && chr <= 'z') || chr == '-' || chr == '.' || chr == '_' || chr == '~'))
-            result += std::string("%") + hex_chars[static_cast<unsigned char>(chr) >> 4] + hex_chars[static_cast<unsigned char>(chr) & 15];
+        if (!((chr >= '0' && chr <= '9') || (chr >= 'A' && chr <= 'Z') || (chr >= 'a' && chr <= 'z') || chr == '-' ||
+              chr == '.' || chr == '_' || chr == '~'))
+            result += std::string("%") + hex_chars[static_cast<unsigned char>(chr) >> 4] +
+                      hex_chars[static_cast<unsigned char>(chr) & 15];
         else
             result += chr;
     }
@@ -85,8 +85,7 @@ inline static std::string url_decode(const std::string &value) noexcept {
             auto decoded_chr = static_cast<char>(std::strtol(hex.c_str(), nullptr, 16));
             result += decoded_chr;
             i += 2;
-        }
-        else if (chr == '+')
+        } else if (chr == '+')
             result += ' ';
         else
             result += chr;
@@ -95,24 +94,19 @@ inline static std::string url_decode(const std::string &value) noexcept {
     return result;
 }
 
-inline static  std::string u8wstring_to_string(const std::wstring& wstr)
-{
+inline static std::string u8wstring_to_string(const std::wstring &wstr) {
     std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
     return conv.to_bytes(wstr);
 }
 
-inline static std::wstring u8string_to_wstring(const std::string& str)
-{
+inline static std::wstring u8string_to_wstring(const std::string &str) {
     std::wstring_convert<std::codecvt_utf8<wchar_t> > conv;
     return conv.from_bytes(str);
 }
 
 
-
-
-std::pair<bool, bool> urlMatch(std::string requestUrl, std::string handlerUrl,
-    std::map<std::string, std::string>& storeMap)
-{
+inline static std::pair<bool, bool> urlMatch(std::string requestUrl, std::string handlerUrl,
+                                      std::map<std::string, std::string> &storeMap) {
     if (requestUrl.find("/static") == 0)
         return std::make_pair(false, true);
 
@@ -127,8 +121,8 @@ std::pair<bool, bool> urlMatch(std::string requestUrl, std::string handlerUrl,
     auto requestUrlLength = requestUrl.length();
     auto max_length = (handlerUrlLength > requestUrlLength) ? handlerUrlLength
                                                             : requestUrlLength;
-    size_t i = 0, j = 0; 
-    for ( ; (i < max_length) && (j < max_length); ++i, ++j) {
+    size_t i = 0, j = 0;
+    for (; (i < max_length) && (j < max_length); ++i, ++j) {
         if (handlerUrl[i] == '{') {
             auto tempIndex = i + 1;
             bool flag = true; // true代表当前在type域中
@@ -174,12 +168,11 @@ std::pair<bool, bool> urlMatch(std::string requestUrl, std::string handlerUrl,
             return std::make_pair(false, false);
         }
     }
-    if(j < requestUrlLength ) return std::make_pair(false,false);
+    if (j < requestUrlLength) return std::make_pair(false, false);
     return std::make_pair(true, false);
 }
 
-std::map<std::string, std::string> split_query_string(std::string str)
-{
+std::map<std::string, std::string> split_query_string(std::string str) {
     std::map<std::string, std::string> results;
     std::string key, val;
     auto flag = true;
@@ -206,8 +199,7 @@ std::map<std::string, std::string> split_query_string(std::string str)
     return results;
 }
 
-std::pair<std::string, std::string> parse_header(std::string& header)
-{
+inline std::pair<std::string, std::string> parse_header(std::string &header) {
     std::string header_key, header_val;
 
     header_val.reserve(50); // pre alloc space
@@ -219,9 +211,8 @@ std::pair<std::string, std::string> parse_header(std::string& header)
     return std::make_pair(header_key, header_val);
 }
 
-std::pair<std::string, std::pair<std::string, std::string>> parse_request_line(
-    std::string request_line)
-{
+inline static std::pair<std::string, std::pair<std::string, std::string>> parse_request_line(
+        std::string& request_line) {
     std::string method, url, version;
 
     url.reserve(50); // pre alloc 50 byte
@@ -253,14 +244,13 @@ std::pair<std::string, std::pair<std::string, std::string>> parse_request_line(
         }
     }
 
-    
+
     // for return three value
     auto url_version_pair = std::make_pair(url_decode(url), version);
     return std::make_pair(method, url_version_pair);
 }
 
-std::string getFileExtension(std::string path)
-{
+std::string getFileExtension(std::string path) {
     auto pos = path.find_last_of('.');
     std::string ext = path.substr(pos + 1);
     std::transform(ext.begin(), ext.end(), ext.begin(), [](char in) -> char {
@@ -271,8 +261,7 @@ std::string getFileExtension(std::string path)
     return ext;
 }
 
-int is_file_exist(const char* path)
-{
+int is_file_exist(const char *path) {
     if (path == NULL)
         return -1;
     if (access(path, F_OK) == 0) {
@@ -281,8 +270,7 @@ int is_file_exist(const char* path)
     return -1;
 }
 
-std::vector<std::string> split(const std::string& s, char delimiter)
-{
+inline static std::vector<std::string> split(const std::string &s, char delimiter) {
     std::vector<std::string> tokens;
     std::string token;
     std::istringstream tokenStream(s);
@@ -293,94 +281,93 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 }
 
 
-
-static const std::string base64_chars = 
-             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-             "abcdefghijklmnopqrstuvwxyz"
-             "0123456789+/";
+static const std::string base64_chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
 
 
 static inline bool is_base64(unsigned char c) {
-  return (isalnum(c) || (c == '+') || (c == '/'));
+    return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
-  std::string ret;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
+std::string base64_encode(unsigned char const *bytes_to_encode, unsigned int in_len) {
+    std::string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
 
-  while (in_len--) {
-    char_array_3[i++] = *(bytes_to_encode++);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
-      i = 0;
+            for (i = 0; (i < 4); i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
     }
-  }
 
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
+    if (i) {
+        for (j = i; j < 3; j++)
+            char_array_3[j] = '\0';
 
-    char_array_4[0] = ( char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
 
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
+        for (j = 0; (j < i + 1); j++)
+            ret += base64_chars[char_array_4[j]];
 
-    while((i++ < 3))
-      ret += '=';
+        while ((i++ < 3))
+            ret += '=';
 
-  }
+    }
 
-  return ret;
+    return ret;
 
 }
 
-std::string base64_decode(std::string const& encoded_string) {
-  int in_len = encoded_string.size();
-  int i = 0;
-  int j = 0;
-  int in_ = 0;
-  unsigned char char_array_4[4], char_array_3[3];
-  std::string ret;
+std::string base64_decode(std::string const &encoded_string) {
+    int in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    std::string ret;
 
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars.find(char_array_4[i]);
+    while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+        char_array_4[i++] = encoded_string[in_];
+        in_++;
+        if (i == 4) {
+            for (i = 0; i < 4; i++)
+                char_array_4[i] = base64_chars.find(char_array_4[i]);
 
-      char_array_3[0] = ( char_array_4[0] << 2       ) + ((char_array_4[1] & 0x30) >> 4);
-      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) +   char_array_4[3];
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
+            for (i = 0; (i < 3); i++)
+                ret += char_array_3[i];
+            i = 0;
+        }
     }
-  }
 
-  if (i) {
-    for (j = 0; j < i; j++)
-      char_array_4[j] = base64_chars.find(char_array_4[j]);
+    if (i) {
+        for (j = 0; j < i; j++)
+            char_array_4[j] = base64_chars.find(char_array_4[j]);
 
-    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
 
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-  }
+        for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    }
 
-  return ret;
+    return ret;
 }
 
 #endif
